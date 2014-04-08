b5f1e92880e87496c0c29558f962ffcc9b36da39
sunyzc <sunyzc@gmail.com>
2014-04-08 15:53:52 +0800

    add javamail test

---
+++ src/main/java/com/sunyzc/ssh/mail/Test.java
@@ -0,0 +1,31 @@
+package com.sunyzc.ssh.mail;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Test {
+	public static void main(String[] args) throws MessagingException {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.163.com");
+		props.put("mail.smtp.auth", "true");
+		Session session = Session.getDefaultInstance(props, new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("sunyzc", "password...");
+			}
+		});
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress("sunyzc@163.com"));
+		message.setRecipient(Message.RecipientType.TO, new InternetAddress("sunyzc@163.com"));
+		message.setSubject("标题");
+		message.setText("邮件正文......");
+		Transport.send(message);
+	}
+}
